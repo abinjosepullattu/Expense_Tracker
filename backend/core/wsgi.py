e1bba578e321eb815e87dbfeb6634a5afdbf0f11
abinjose <146512469+abinjosepullattu@users.noreply.github.com>
2026-07-16 22:29:33 +0530

    3rd commit

--- backend/core/wsgi.py
+++ backend/core/wsgi.py
@@ -1,5 +1,9 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.development')
+# If running on Render, default to production settings
+if os.environ.get('RENDER'):
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.production')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.development')
 application = get_wsgi_application()
--- backend/manage.py
+++ backend/manage.py
@@ -6,7 +6,11 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.development')
+    # If running on Render, default to production settings
+    if os.environ.get('RENDER'):
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.production')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.development')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
